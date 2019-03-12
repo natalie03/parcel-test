8d71ec6d196993c750e1b55788bf21904d2c5b08
Natalie <nataliemiller03@gmail.com>
2019-03-12 12:50:52 -0400

    Switch to react

--- components/App.js
+++ components/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 
-import './index.scss';
-
 function App() {
   const [name, setName] = useState('Jeff');
   return (
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import App from './components/App';
+import './index.scss';
 
 let app = document.getElementById('app');
 if (app) {
